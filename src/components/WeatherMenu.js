dadddbc76cd4cb8d38610c66ddb9128f8c248df0
Anes Sheremeti <anessheremeti9@gmail.com>
2023-08-16 11:31:57 +0200

    Deployed website

--- src/components/WeatherMenu.js
+++ src/components/WeatherMenu.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect} from "react";
 import classes from "./WeatherMenu.module.css";
 
 const WeatherMenu = () => {
